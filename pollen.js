02bc3619495974bf52888137e9e6ba12c8087e3e
yvesHiernaux <60437657+yvesHiernaux@users.noreply.github.com>
2025-12-10 17:43:32 +0100

    Documentation edits made through Mintlify web editor

---
+++ pollen.js
@@ -0,0 +1,1 @@
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://load.pollen-qa.beebole.com/elxxvtjls.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','10=GghXMyI1VVk4NllGREs2SgVRWFpHXhwCTg4PERQXFAldDwUZ');
